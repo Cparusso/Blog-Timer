5bf55f9844345b630822a68e03b0675fc4d49d59
github username <github email address>
2019-06-28 16:22:21 -0400

    Added an overtime feature

--- src/App.js
+++ src/App.js
@@ -1,54 +1,93 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-class App extends React.Component {
+class App extends Component {
   state = {
-    seconds: 0,
-    minutes: 5,
+    seconds: 5, // Currently when the timer goes into overtime it waits until 2 seconds to update the time in the title
+    // seconds: 0,
+    minutes: 0,
+    // minutes: 5,
     timerRunning: false,
+    overtime: false,
   }
 
   updateTitle = () => {
-    const { seconds, minutes } = this.state
-
-    document.title = `Time: ${minutes}:${seconds < 10 ? `0${seconds}` : seconds}`
+    const { seconds, minutes, overtime } = this.state
+    document.title = `${overtime ? `Overtime` : `Time` }: ${minutes}:${seconds < 10 ? `0${seconds}` : seconds}`
   }
 
   toggleTimer = () => {
-    const { timerRunning } = this.state
+    const { timerRunning, overtime } = this.state
 
-    if (!timerRunning) {
-      this.setState((prevState) => ({
-        timerRunning: !prevState.timerRunning
-      }))
+    if (!overtime) {
+      if (!timerRunning) {
+        this.setState((prevState) => ({
+          timerRunning: !prevState.timerRunning
+        }))
 
-      this.myInterval = setInterval(() => {
-        const { seconds, minutes } = this.state
+        this.myInterval = setInterval(() => {
+          const { seconds, minutes } = this.state
 
-        if (seconds > 0) {
-          this.setState(({ seconds }) => ({
-            seconds: seconds - 1
-          }), this.updateTitle)
-        }
-        if (seconds === 0) {
-          if (minutes === 0) {
-            clearInterval(this.myInterval)
-          } else {
-            this.setState(({ minutes }) => ({
-              minutes: minutes - 1,
-              seconds: 59
+          if (seconds > 0) {
+            this.setState((prevState) => ({
+              seconds: prevState.seconds - 1
             }), this.updateTitle)
           }
-        }
-      }, 1000)
+          if (seconds === 0) {
+            if (minutes === 0) {
+              clearInterval(this.myInterval)
+              this.setState((prevState) => ({
+                overtime: !prevState.overtime
+              }))
+              this.countUp()
+            } else {
+              this.setState((prevState) => ({
+                minutes: prevState.minutes - 1,
+                seconds: 59
+              }), this.updateTitle)
+            }
+          }
+        }, 1000)
+      } else {
+        clearInterval(this.myInterval)
+
+        this.setState((prevState) => ({
+          timerRunning: !prevState.timerRunning
+        }))
+      }
     } else {
       clearInterval(this.myInterval)
 
       this.setState((prevState) => ({
-        timerRunning: !prevState.timerRunning
+        timerRunning: false,
+        seconds: 0,
+        minutes: 5,
+        overtime: false,
       }))
     }
+  }
+
+  countUp = () => {
+    clearInterval(this.myInterval)
+
+    this.myInterval = setInterval(() => {
+      const { seconds } = this.state
+
+      if (seconds < 60) {
+        this.setState((prevState) => ({
+          seconds: prevState.seconds + 1
+        }), this.updateTitle)
+      }
+      if (seconds === 59) {
+        this.setState((prevState) => ({
+          minutes: prevState.minutes + 1,
+          seconds: 0
+        }), this.updateTitle)}
+      }, 1000)
+
+    this.setState((prevState) => ({
+      seconds: prevState.seconds + 1
+    }))
   }
 
   componentWillUnmount() {
@@ -60,9 +99,9 @@ class App extends React.Component {
     return (
       <div>
         <div className="header">
-          <p id="title"><span id="emphasize">//</span> Blog Timer</p>
+          <p id="title"><span id="emphasize">{`//`}</span> Blog Timer</p>
           <p id="time-remaining">
-            Time Remaining: <span class={ timerRunning ? "on" : "off" }> {minutes}:{seconds < 10 ? `0${seconds}` : seconds}</span>
+            Time Remaining: <span className={ timerRunning ? "on" : "off" }> {minutes}:{seconds < 10 ? `0${seconds}` : seconds}</span>
           </p>
           <button id="timer-button" onClick={() => this.toggleTimer()}>
             { timerRunning ? "Stop Timer" : "Start Timer" }
@@ -75,6 +114,9 @@ class App extends React.Component {
 
 export default App;
 
+// import React, { useState, useEffect } from 'react';
+// import './App.css';
+//
 // function App() {
 //   console.log("App function called")
 //   let [minutes, setMinutes] = useState(5);
